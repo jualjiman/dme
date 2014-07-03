c617a80b13696ad51fc54ba30b20394f55fbc68a
jualjiman <jualjiman@gmail.com>
2014-07-02 23:56:53 -0500

    just a little more details and thats all

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,34 @@
+$(function(){
+	initialize();
+});
+
+function initialize() {
+
+	var ubicacion = new google.maps.LatLng(19.389027, -99.165297);
+
+	var mapOptions = {
+	  	center: ubicacion,
+	  	zoom: 15,
+	  	mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var map = new google.maps.Map($("#map-content")[0], mapOptions);
+
+	var marker = new google.maps.Marker({
+	      position: ubicacion,
+	      map: map,
+	      title: 'EMD México'
+	});
+}
+
+//resizeable navbar menu
 $(document).on("scroll",function(){
     if($(document).scrollTop()>100){
         $("nav").addClass("small");
+        // $("#mainContent").addClass("smallTopAutoPadding").removeClass("topAutoPadding");
+
     } else{
         $("nav").removeClass("small");
+        // $("#mainContent").addClass("topAutoPadding").removeClass("smallTopAutoPadding");
     }
 });
