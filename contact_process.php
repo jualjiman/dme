9c693d58f8623322f9b34be6a4a0920014c97d05
jualjiman <jualjiman@gmail.com>
2014-07-09 21:29:05 -0500

    info

--- contact_process.php
+++ contact_process.php
@@ -1,10 +1,11 @@
 <?php
-	$emailto = "Juan Alberto Jimenez Angel <jualjiman@gmail.com>";
+	$emailto = "Juan Alberto Jimenez Angel <jualjiman@gmail.com>, Juan Alberto Jimenez <blow.it.away@live.com.mx>";//"Alejandro Zarco González <alejandrozarco@emdmexico.com>, Alejandro Villamizar <alejandrovillamizar@emdmexico.com>";
+	//
 	$subject = "Contacto emdmexico.com";
 	
 	$name = $_POST["name"];
 	$email = $_POST["email"];
-	$message = $name." Mensaje desde emdmexico.com.\nMensaje:\n ".$_POST["message"]."\n\nInformación del remitente mensaje:\nNombre:\n ".$name."\n\nEmail:\n ".$email;
+	$message = "Mensaje desde emdmexico.com.\n\n ".$_POST["message"]."\n\n-------------------------------\nInformación del remitente:\n\nNombre:\n ".$name."\n\nEmail:\n ".$email;
 
 	$headers   = array();
 	$headers[] = "MIME-Version: 1.0";
