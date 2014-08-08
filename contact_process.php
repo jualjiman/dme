43f9744716265aeb9e477c3e81dea28c3aac8166
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-08-08 09:56:27 -0500

    agregando Google Analytics

--- contact_process.php
+++ contact_process.php
@@ -1,17 +1,16 @@
 <?php
-	$emailto = "Juan Alberto Jimenez Angel <jualjiman@gmail.com>";//, Juan Alberto Jimenez <blow.it.away@live.com.mx>";//"Alejandro Zarco González <alejandrozarco@emdmexico.com>, Alejandro Villamizar <alejandrovillamizar@emdmexico.com>";
+	$emailto = "Alejandro Villamizar <Contacto.emdmexico@gmail.com>";//, Juan Alberto Jimenez <blow.it.away@live.com.mx>";//"Alejandro Zarco González <alejandrozarco@emdmexico.com>, Alejandro Villamizar <alejandrovillamizar@emdmexico.com>";
 	//
-	$subject = "Contacto emdmexico.com";
+	$subject = "Nuevo mensaje de contacto EMDMexico.com";
 	
 	$name = $_POST["name"];
 	$email = $_POST["email"];
-	$message = "Mensaje desde emdmexico.com.\n\n ".$_POST["message"]."\n\n-------------------------------\nInformación del remitente:\n\nNombre:\n ".$name."\n\nEmail:\n ".$email;
+	$message = "\n\n ".$_POST["message"]."\n\n-------------------------------------------------------------------\nInformación del remitente:\n\nNombre:\n ".$name."\n\nEmail:\n ".$email;
 
 	$headers   = array();
 	$headers[] = "MIME-Version: 1.0";
 	$headers[] = "Content-type: text/plain; charset=iso-8859-1";
 	$headers[] = "From: ".$name." <".$email.">";
-	$headers[] = "Subject: ".$subject;
 	$headers[] = "X-Mailer: PHP/".phpversion();
 	
 	$message_info = mail( $emailto, $subject, $message, implode( "\r\n",$headers ) );
